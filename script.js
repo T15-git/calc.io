74c12d9dc25a9baf68317656dd201b0f5e117c9a
tushar <tushar.code05@gmail.com>
2021-05-23 10:30:07 +1100

    updated

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
-    function plus() {
+    var choice;
+
+    
+function num(){
+    console.log(choice);
+
+    if(choice==1){
+        document.getElementById('number2').value += '1'
+    }
+    else if(choice==2){
+        document.getElementById('number2').value += '2'
+    }
+    else if(choice==3){
+        document.getElementById('number2').value += '3'
+    }
+    else if(choice==4){
+        document.getElementById('number2').value += '4'
+    }
+    else if(choice==5){
+        document.getElementById('number2').value += '5'
+    }
+    else if(choice==6){
+        document.getElementById('number2').value += '6'
+    }
+    else if(choice==7){
+        document.getElementById('number2').value += '7'
+    }
+    else if(choice==8){
+        document.getElementById('number2').value += '8'
+    }
+    else if(choice==9){
+        document.getElementById('number2').value += '9'
+    }
+    else if(choice==0){
+        document.getElementById('number2').value += '0'
+    }
+    else if(choice=='10'){
+        document.getElementById('number2').value += '00'
+    }
+    else if(choice=='+'){
         document.getElementById('number2').value += '+'
     }
+    else if(choice=='-'){
+        document.getElementById('number2').value += '-'
+    }
+    else if(choice=='*'){
+        document.getElementById('number2').value += '*'
+    }
+    else if(choice=='/'){
+        document.getElementById('number2').value += '/'
+    }
+    else if(choice=='%'){
+        document.getElementById('number2').value += '%'
+    }
+    else if(choice=='c'){
+        document.getElementById('number2').value += 'c'
+    }
+    else if(choice=='='){
+        document.getElementById('number2').value += '='
+    }
+    
+    
+}
