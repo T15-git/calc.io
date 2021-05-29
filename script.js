ed5114ea4cfff35970498e148ef3a45130c66752
Ketan Tetry <ketan@signitysolutions.com>
2021-05-30 00:08:46 +0530

    design front end and fix minor bug

--- script.js
+++ script.js
@@ -3,90 +3,105 @@ var index = 0;
 var currentNumber = "";
 
 function clean() {
-    document.getElementById('input_field').value = ""
-    expression = [];
-    index = 0;
-    currentNumber = "";
+  document.getElementById("input_field").value = "";
+  expression = [];
+  index = 0;
+  currentNumber = "";
 }
 
 function number(num) {
-    document.getElementById('input_field').value += num;
-    currentNumber += num;
+  document.getElementById("input_field").value += num;
+  currentNumber += num;
 }
 
 function dott() {
+  if (document.getElementById("input_field").value == "") {
+    return;
+  }
 
-    if (document.getElementById('input_field').value == "") {
-        return;
-    }
-
-    if (!currentNumber.includes(".") && currentNumber != "") {
-        currentNumber += ".";
-        document.getElementById('input_field').value += ".";
-    }
+  if (!currentNumber.includes(".") && currentNumber != "") {
+    currentNumber += ".";
+    document.getElementById("input_field").value += ".";
+  }
 }
 
-function operator(op) {    
+function operator(op) {
+  if (document.getElementById("input_field").value == "") {
+    return;
+  }
 
-    if (document.getElementById('input_field').value == "") {
-        return;
-    }
+  if (currentNumber == "") {
+    var oldval = document.getElementById("input_field").value;
+    document.getElementById("input_field").value = oldval.replaceAt(
+      oldval.length - 1,
+      op
+    );
+    expression[--index] = op;
+  } else {
+    document.getElementById("input_field").value += op;
+    expression[index] = currentNumber;
+    expression[++index] = op;
+  }
 
-    if (currentNumber == "") {
-        var oldval = document.getElementById('input_field').value;
-        document.getElementById('input_field').value = oldval.replaceAt(oldval.length - 1, op);
-        expression[--index] = op;
-    } else {
-        document.getElementById('input_field').value += op;
-        expression[index] = currentNumber;
-        expression[++index] = op;
-    }
-    
-    index++;
-    currentNumber = "";
+  index++;
+  currentNumber = "";
 }
 
 function response() {
+  if (document.getElementById("input_field").value == "") {
+    return;
+  }
 
-    if (document.getElementById('input_field').value == "") {
-        return;
-    }
+  if (currentNumber != "") {
+    expression[index] = currentNumber;
+  }
 
-    if (currentNumber != "") {
-        expression[index] = currentNumber;
-    }
+  var lastValue = expression[expression.length - 1];
+  if (lastValue == "." || isOperator(lastValue)) {
+    return;
+  }
 
-    var lastValue = expression[expression.length - 1];
-    if (lastValue == "." || isOperator(lastValue)) {
-        return;
-    }
+  while (expression.length != 1) {
+    var i = expression.length - 1;
 
-    while (expression.length != 1) {
-        var i = expression.length - 1;
+    var firstOperand = parseFloat(expression[i]);
+    var operator = expression[i - 1];
+    var secondOperand = parseFloat(expression[i - 2]);
 
-        var firstOperand = parseFloat(expression[i]);
-        var operator = expression[i - 1];
-        var secondOperand = parseFloat(expression[i - 2]);
+    var result = calculate(firstOperand, secondOperand, operator);
+    var slicedArray = expression.slice(0, expression.length - 3);
+    expression = slicedArray;
+    expression[i - 2] = result;
+  }
 
-        var result = calculate(firstOperand, secondOperand, operator);
-        var slicedArray = expression.slice(0, expression.length - 3);;
-        expression = slicedArray;
-        expression[i - 2] = result;
-    }
-
-    document.getElementById('input_field').value = expression[0];
+  document.getElementById("input_field").value = expression[0];
+  currentNumber = expression[0];
+  index = 0;
+  expression = [];
 }
 
 function calculate(firstOperand, secondOperand, operator) {
-    switch (operator) {
-        case "+": return firstOperand + secondOperand;
-        case "-": if (firstOperand > secondOperand) return firstOperand - secondOperand; else return secondOperand - firstOperand;
-        case "*": return firstOperand * secondOperand;
-        case "/": return firstOperand / secondOperand;
-        case "%": return firstOperand % secondOperand;
-    }
+  switch (operator) {
+    case "+":
+      return firstOperand + secondOperand;
+    case "-":
+      if (firstOperand > secondOperand) return firstOperand - secondOperand;
+      else return secondOperand - firstOperand;
+    case "*":
+      return firstOperand * secondOperand;
+    case "/":
+      return firstOperand / secondOperand;
+    case "%":
+      return firstOperand % secondOperand;
+  }
 }
 
 function isOperator(operator) {
-    return operator == "*" || operator == "/" || operator == "-" || operator == "+" || operator == "%";
+  return (
+    operator == "*" ||
+    operator == "/" ||
+    operator == "-" ||
+    operator == "+" ||
+    operator == "%"
+  );
 }
