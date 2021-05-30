d19cc6ff2a021c227ad61b81a93c7c6e2bba4068
Tushar <tushar.code05@gmail.com>
2021-05-31 10:22:37 +1100

    updated

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 var i = 0;
 var save = [];
 
+function AC(){
+    document.getElementById('text').value = '';
+    save = [];
+    i=0;
+}
+
 function letter(num){
     document.getElementById('text').value += num;
     if(save[i]==null) save[i] = num; else save[i] += num;
 }
 function back(){
-    if(save[i]==null) return;
-    else{
-        save[i] = save[i].substring(0,save[i].length-1);
-        let str = document.getElementById('text').value;
-        str= str.substring(0,str.length-1);
-        document.getElementById('text').value = str;
-    }
+    if(save[i]==null || save[i]=='') return;
+    if(i==0) save[0]=document.getElementById('text').value;
+
+    save[i] = cutlast(save[i]);
+    let str = document.getElementById('text').value;
+    str = cutlast(str);
+    document.getElementById('text').value = str;
+
 }
 
 function optr(op){
-    if(save[0]==null && op == '-'){
+    if((save[0]==null || save[0]=='') && op == '-'){
         document.getElementById('text').value += op;
         save[0]='-';
+        return;
     }
-    else if (save[0]=='' || save[0]== '-') return;
+    if (save[0]=='' || save[0]== '-') return;
 
-    else if(save[i]==''){
+    if(save[i]==null || save[i]==''){
         let rep = document.getElementById('text').value
         document.getElementById('text').value = rep.replaceAt(rep.length - 1, op);
         op=checkop(op);
         save[i-1]=op;
+        return;
     }
-    else if (save[i].charAt(save[i].length-1)=='.') return;
+    let dot = checkdot(save[i])
+    if (save[i][save[i].length-1] =='.') return;
     else{
         document.getElementById('text').value += op;
         op=checkop(op);
@@ -45,7 +55,7 @@ function dot(){
 }
 
 function result(){
-    if(save[i]==null || save[i].charAt(save[i].length-1) == '.') return;
+    if((save[i]==null || save[i]=='') || save[i].charAt(save[i].length-1) == '.') return;
 
     while(i!=0){
         save[i-2]= calculate(save[i]=parseFloat(save[i]) , save[--i] , save[--i]=parseFloat(save[i]))
@@ -68,6 +78,22 @@ function checkop(op){
     if(op=='x') return '*';
     else if(op=='÷') return '/';
     else return op;
+}
+
+function checkdot(str){
+    str =str[str.length-1];
+    return str;
+}
+function cutlast(str){
+    let index = str.length-1;
+    let newstr='';
+    let i=0;
+
+    while(i!=index){
+        newstr+=str[i];
+        i++;
+    }
+return newstr;
 }
 
 String.prototype.replaceAt = function(index, replacement) {
